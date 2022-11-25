b7b69aa83351e53aadc2bf8fef26036b06de0499
Sartorius11 <sartoxr@gmail.com>
2022-11-26 00:01:18 +0100

    Commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <BulmaComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import BulmaComponent  from './components/BulmaComponent.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BulmaComponent
   }
 }
 </script>
